07d1d1d8a33e9144614619ba52be675cbcd49317
jmgpena <jmgpena@gmail.com>
2024-07-24 14:48:48 +0100

    Finish kickstart.nvim conversion

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -16,6 +16,7 @@ vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
 vim.keymap.set("n", "<leader>de", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 vim.keymap.set("n", "<leader>dq", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
 vim.keymap.set("n", "<tab>", "za") -- Toggle fold region
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
